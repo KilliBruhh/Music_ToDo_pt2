b36580a8e7e74e7b8ae1a64f51eb2778020432a4
KilliBruhh <61047380+KilliBruhh@users.noreply.github.com>
2022-12-30 16:18:51 +0100

    Testing complete
    5 tests

--- app/src/androidTest/java/com/example/musicapp_project_appdev/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/musicapp_project_appdev/ExampleInstrumentedTest.java
@@ -2,14 +2,19 @@ package com.example.musicapp_project_appdev;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import android.app.Activity;
 import android.content.Context;
 
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -17,6 +22,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.*;
 
 /**
@@ -38,9 +44,6 @@ public class ExampleInstrumentedTest {
 
 
     // TEst navigation form main to side activities
-    // app closes at end of test --> Is this normal? (no error message in test logs)
-    // Works
-
     @Test
     public void testSettingsActivity() {
         ActivityScenario mainActivityScenario = ActivityScenario.launch(MainActivity.class);
@@ -52,15 +55,44 @@ public class ExampleInstrumentedTest {
     public void testAddMusicActivity() {
         ActivityScenario mainActivityScenario = ActivityScenario.launch(MainActivity.class);
         onView(withId(R.id.goAddSong)).perform(click());
-        // Check if Settings activity is now viewable
+        // Check if Add song activity is now viewable
         onView(withId(R.id.addSongActivity)).check(matches(isDisplayed()));
     }
 
+    @Test
+    public void testAddSong() {
+        ActivityScenario mainActivityScenario = ActivityScenario.launch(MainActivity.class);
+        onView(withId(R.id.goAddSong)).perform(click());
+
+        onView(withId(R.id.songName)).perform(replaceText("Test Song"));
+        onView(withId(R.id.AlbumName)).perform(replaceText("Test Album"));
+        onView(withId(R.id.durationSong)).perform(replaceText("111")).perform(closeSoftKeyboard());
+
+        onView(withId(R.id.addSongButton)).perform(click());
+
+    }
+
     // Test when u want to edit the editText fields are not empty
+    @Test
     public void testUpdateFields() {
         ActivityScenario mainActivityScenario = ActivityScenario.launch(MainActivity.class);
-        onView(withId(R.id.goSettings)).perform(click());
-        // Check if Settings activity is now viewable
-        onView(withId(R.id.backgroundId)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.editButton)).perform(click());
+
+        onView(withId(R.id.EditSongName)).check(matches(withText("Test Song")));
+        onView(withId(R.id.EditAlbumSong)).check(matches(withText("Test Album")));
+        onView(withId(R.id.EditDurationSong)).check(matches(withText("111"))).perform(closeSoftKeyboard());
+
+        onView(withId(R.id.EditSongButton)).perform(click());
+    }
+
+    // Test Delete
+    @Test
+    public void testDeleteSong() {
+        ActivityScenario mainActivityScenario = ActivityScenario.launch(MainActivity.class);
+
+        onView(withId(R.id.editButton)).perform(click());
+
+        onView(withId(R.id.DeleteSongButton)).perform(click());
     }
 }
