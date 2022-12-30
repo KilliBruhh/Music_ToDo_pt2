e72a44ee6aae8699c7195cee8f480c428979e63f
KilliBruhh <61047380+KilliBruhh@users.noreply.github.com>
2022-12-30 13:45:09 +0100

    2 nav tests

--- app/src/androidTest/java/com/example/musicapp_project_appdev/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/musicapp_project_appdev/ExampleInstrumentedTest.java
@@ -1,10 +1,19 @@
 package com.example.musicapp_project_appdev;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import android.app.Activity;
 import android.content.Context;
 
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -17,10 +26,41 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    /*
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.musicapp_project_appdev", appContext.getPackageName());
+    }
+    */
+
+
+    // TEst navigation form main to side activities
+    // app closes at end of test --> Is this normal? (no error message in test logs)
+    // Works
+
+    @Test
+    public void testSettingsActivity() {
+        ActivityScenario mainActivityScenario = ActivityScenario.launch(MainActivity.class);
+        onView(withId(R.id.goSettings)).perform(click());
+        // Check if Settings activity is now viewable
+        onView(withId(R.id.backgroundId)).check(matches(isDisplayed()));
+    }
+    @Test
+    public void testAddMusicActivity() {
+        ActivityScenario mainActivityScenario = ActivityScenario.launch(MainActivity.class);
+        onView(withId(R.id.goAddSong)).perform(click());
+        // Check if Settings activity is now viewable
+        onView(withId(R.id.addSongActivity)).check(matches(isDisplayed()));
+    }
+
+    // Test when u want to edit the editText fields are not empty
+    public void testUpdateFields() {
+        ActivityScenario mainActivityScenario = ActivityScenario.launch(MainActivity.class);
+        onView(withId(R.id.goSettings)).perform(click());
+        // Check if Settings activity is now viewable
+        onView(withId(R.id.backgroundId)).check(matches(isDisplayed()));
     }
 }
